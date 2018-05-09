df699cdae19ba550de8d92690c38ff707e2e6cea
dragonbook <573260700@qq.com>
2018-05-10 03:05:51 +0800

    acc to 0.77, with simple small filter net

--- assignment2/my/models.py
+++ assignment2/my/models.py
@@ -52,7 +52,7 @@ class ExampleDropoutNet(nn.Module):
             Flatten(),
             nn.Linear(32*13*13, 1024),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(p=0.5),
             nn.Linear(1024, 10)
         )
 
@@ -86,4 +86,33 @@ class SmallFilterNet(nn.Module):
     def forward(self, x):
         features = self.features(x)
         return self.classifier(features)
+
+
+class SmallFilterDropoutNet(nn.Module):
+    def __init__(self):
+        super(SmallFilterDropoutNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            Flatten(),
+            nn.Linear(64*8*8, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(1024, 10)
+        )
+
+    def forward(self, x):
+        features = self.features(x)
+        return self.classifier(features)
+
 
